9c8d659981639d897d4bdbb69da17117b8b30c8a
Ayat-olimat <82057557+Ayat-olimat@users.noreply.github.com>
2021-06-08 13:29:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+const path = require("path")
+const fs = require("fs")
+
+app.use(express.static(__dirname + '/public'));
+
+const bodyParserMW = bodyParser.urlencoded({
+    extended: true
+})
+app.get("/", (req, res, next)=>{
+  res.sendFile(path.join(__dirname,"..","express","home.html"))
+})
+app.get("/form", (req, res , next)=>{
+  res.sendFile(path.join(__dirname,"..","express","index.html"))
+})
+app.post("/SignUP", bodyParserMW, (req, res, next)=>{
+    console.log(req.body)
+    fs.writeFileSync("programming.txt" ,JSON.stringify(req.body));
+    res.end();
+})
+app.listen(3000, ()=>{
+    console.log("listening on 3000...")
+})
